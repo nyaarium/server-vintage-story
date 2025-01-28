19e440ac0eb1829ceee3e2042df7adbbda28e8d5
Nyaarium <project@nyaarium.com>
2025-01-27 18:20:42 -0800

    Properly cd to correct directory

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+cd "$(dirname "$0")"
+
 export APP_SERVICE=true
 ./run.sh
 docker logs -f vintage-story
--- stop.sh
+++ stop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+set -e
+cd "$(dirname "$0")"
+
 docker kill --signal=SIGTERM vintage-story
